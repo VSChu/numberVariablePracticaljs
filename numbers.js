2043c173d9c0615442f7a37d82f8a3bdc42623e3
Vithue Chumara <vithuecs@gmail.com>
2020-07-16 21:20:12 -0500

    first commit

---
+++ numbers.js
@@ -0,0 +1,34 @@
+console.log(' js ');
+
+let motorcycles = 9;
+let cars = 0;
+let vehicles = motorcycles + cars;
+console.log('Bikes: ', motorcycles);
+console.log('Cars: ', cars);
+console.log('Total Vehicles: ', vehicles);
+console.log('Had to put the soft tail in the garage');
+
+//decrement motorcycles
+motorcycles--; //will subtract 1 from variable
+
+console.log('Miised my bike so I bought 2 Lambos');
+cars = cars += 2;
+console.log('Bikes: ', motorcycles);
+console.log('Cars: ', cars);
+vehicles = motorcycles + cars;
+console.log('Total Vehicles: ', vehicles);
+console.log('I wanted a Jetta so I got one');
+cars++;
+console.log('I had to sell 3 bikes');
+motorcycles -= 3;
+console.log('Bikes: ', motorcycles);
+console.log('Cars: ', cars);
+vehicles = motorcycles + cars;
+console.log('Total Vehicles: ', vehicles);
+
+console.log("Got the soft tail back from the garage");
+motorcycles ++;
+console.log('Bikes: ', motorcycles);
+console.log('Cars: ', cars);
+vehicles = motorcycles + cars;
+console.log('Total Vehicles: ', vehicles);
